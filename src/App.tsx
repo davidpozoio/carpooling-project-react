50c76e5333ff8606c3970b843b48d7610d8593bb
davidpozoio <117756174+davidpozoio@users.noreply.github.com>
2024-01-12 12:28:16 -0500

    basic pages added

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,20 @@
+import { Routes, Route, Navigate } from "react-router-dom";
 import "./App.css";
+import Home from "./pages/home/Home";
+import Login from "./pages/auth/Login";
+import Signup from "./pages/auth/Signup";
+import NotFound from "./pages/error/NotFound";
 
 function App() {
   return (
     <>
-      <h1></h1>
+      <Routes>
+        <Route path="/" element={<Navigate to="/home" />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/home/login" element={<Login />} />
+        <Route path="/home/signup" element={<Signup />} />
+        <Route path="*" element={<NotFound/>}/>
+      </Routes>
     </>
   );
 }
---
+++ src/pages/error/NotFound.tsx
@@ -0,0 +1,4 @@
+const NotFound = () => {
+  return <div>Error 404: Page not found</div>;
+};
+export default NotFound;
